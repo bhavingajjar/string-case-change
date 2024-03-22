6edb49b3aa180de00388c8f8405745a594bd9d68
Bhavin Gajjar <gajjarbhavin22@gmail.com>
2024-03-22 12:32:20 +0530

    initial commit

---
+++ test/script.js
@@ -0,0 +1,11 @@
+// test/script.js
+// Importing the class
+const StringCaseChange = require('string-case-change');
+
+// Example usage
+const myString = "hello world";
+
+console.log(StringCaseChange.toLowerCase(myString));
+console.log(StringCaseChange.toUpperCase(myString));
+console.log(StringCaseChange.toTitleCase(myString));
+console.log(StringCaseChange.toSentenceCase(myString));
