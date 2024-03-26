5b799971a89caaa97d4d4f7f8410a55a4dc69b98
Bhavin Gajjar <gajjarbhavin22@gmail.com>
2024-03-26 11:37:39 +0530

    added some more caese

--- README.md
+++ README.md
@@ -10,11 +10,20 @@ const stringCaseChange = require('string-case-change');
 // Example usage
 const myString = "heLlo woRld";
 
-console.log('Lower case: ' + stringCaseChange.toLowerCase(myString));
-console.log('Upper case: ' + stringCaseChange.toUpperCase(myString));
-console.log('Title case: ' + stringCaseChange.toTitleCase(myString));
-console.log('Sentence case: ' + stringCaseChange.toSentenceCase(myString));
-console.log('Camel case: ' + stringCaseChange.toCamelCase(myString));
-console.log('Snake case: ' + stringCaseChange.toSnakeCase(myString));
-console.log('Kebab case: ' + stringCaseChange.toKebabCase(myString));
-console.log('Pascal case: ' + stringCaseChange.toPascalCase(myString));
+console.log('Lower case: ' + stringCaseChange.toLowerCase(myString)); // Lower case: hello world
+console.log('Upper case: ' + stringCaseChange.toUpperCase(myString)); // Upper case: HELLO WORLD
+console.log('Title case: ' + stringCaseChange.toTitleCase(myString)); // Title case: Hello World
+console.log('Sentence case: ' + stringCaseChange.toSentenceCase(myString)); // Sentence case: Hello world
+console.log('Camel case: ' + stringCaseChange.toCamelCase(myString)); // Camel case: helloWorld
+console.log('Snake case: ' + stringCaseChange.toSnakeCase(myString)); // Snake case: hello_world
+console.log('Kebab case: ' + stringCaseChange.toKebabCase(myString)); // Kebab case: hello-world
+console.log('Pascal case: ' + stringCaseChange.toPascalCase(myString)); // Pascal case: HelloWorld
+console.log('Constant case: ' + stringCaseChange.toConstantCase(myString)); // Constant case: HELLO_WORLD
+console.log('Dot case: ' + stringCaseChange.toDotCase(myString)); // Dot case: hello.world
+console.log('Path case: ' + stringCaseChange.toPathCase(myString)); // Path case: hello/world
+console.log('Alternating case: ' + stringCaseChange.toAlternatingCase(myString)); // Alternating case: HeLlO WoRlD
+console.log('Inverted case: ' + stringCaseChange.toInvertedCase(myString)); // Inverted case: HElLO WOrLD
+console.log('Sponge case: ' + stringCaseChange.toSpongeCase(myString)); // Sponge case: HeLLo worLD
+console.log('Reverse case: ' + stringCaseChange.toReverseCase(myString)); // Reverse case: DLrOW OLlEH
+console.log('Small caps case: ' + stringCaseChange.toSmallCaps(myString)); // Small caps case: hello world
+console.log('Mirrored case: ' + stringCaseChange.toMirroredCase(myString)); // Mirrored case: plᴚoʍ ol⅂ǝɥ
--- index.js
+++ index.js
@@ -53,6 +53,134 @@ class stringCaseChange {
             return w[0].toUpperCase() + w.slice(1).toLowerCase();
         }).replace(/\s/g, '');
     }
+
+    // Function to convert string to constant case (UPPERCASE_SNAKE_CASE)
+    static toConstantCase(str) {
+        str = this.toLowerCase(str); // Convert to lowercase first
+        return str.replace(/\s/g, '_').toUpperCase();
+    }
+
+    // Function to convert string to dot case (lowercase.dot.case)
+    static toDotCase(str) {
+        str = this.toLowerCase(str); // Convert to lowercase first
+        return str.replace(/\s/g, '.');
+    }
+
+    // Function to convert string to path case (lowercase/path/case)
+    static toPathCase(str) {
+        str = this.toLowerCase(str); // Convert to lowercase first
+        return str.replace(/\s/g, '/');
+    }
+
+    static toAlternatingCase(str) {
+        return str.split('').map((char, index) => index % 2 === 0 ? char.toUpperCase() : char.toLowerCase()).join('');
+    }
+
+    // Function to convert string to inverted case
+    static toInvertedCase(str) {
+        return str.split('').map(char => char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()).join('');
+    }
+
+    // Function to convert string to sponge case
+    static toSpongeCase(str) {
+        return str.split('').map(char => Math.random() < 0.5 ? char.toUpperCase() : char.toLowerCase()).join('');
+    }
+
+    // Function to convert string to reverse case
+    static toReverseCase(str) {
+        return str.split('').map(char => char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()).reverse().join('');
+    }
+
+    // Function to convert string to small caps
+    static toSmallCaps(str) {
+        return str.split('').map(char => char === char.toUpperCase() ? char.toLowerCase() : char).join('');
+    }
+
+    // Function to convert string to mirrored case
+    static toMirroredCase(str) {
+        const mirroredChars = {
+            a: 'ɐ',
+            b: 'q',
+            c: 'ɔ',
+            d: 'p',
+            e: 'ǝ',
+            f: 'ɟ',
+            g: 'ƃ',
+            h: 'ɥ',
+            i: 'ᴉ',
+            j: 'ɾ',
+            k: 'ʞ',
+            l: 'l',
+            m: 'ɯ',
+            n: 'u',
+            o: 'o',
+            p: 'd',
+            q: 'b',
+            r: 'ɹ',
+            s: 's',
+            t: 'ʇ',
+            u: 'n',
+            v: 'ʌ',
+            w: 'ʍ',
+            x: 'x',
+            y: 'ʎ',
+            z: 'z',
+            A: '∀',
+            B: '𐐒',
+            C: 'Ɔ',
+            D: 'ᗡ',
+            E: 'Ǝ',
+            F: 'Ⅎ',
+            G: '⅁',
+            H: 'H',
+            I: 'I',
+            J: 'ſ',
+            K: '⋊',
+            L: '⅂',
+            M: 'W',
+            N: 'N',
+            O: 'O',
+            P: 'Ԁ',
+            Q: 'Ό',
+            R: 'ᴚ',
+            S: 'S',
+            T: '⊥',
+            U: '∩',
+            V: 'ᴧ',
+            W: 'M',
+            X: 'X',
+            Y: '⅄',
+            Z: 'Z',
+            '0': '0',
+            '1': 'Ɩ',
+            '2': 'ᄅ',
+            '3': 'Ɛ',
+            '4': 'ㄣ',
+            '5': 'ϛ',
+            '6': '9',
+            '7': 'ㄥ',
+            '8': '8',
+            '9': '6',
+            '?': '¿',
+            '!': '¡',
+            '.': '˙',
+            ',': "'",
+            "'": ',',
+            '"': ',,',
+            '`': ',',
+            '(': ')',
+            ')': '(',
+            '[': ']',
+            ']': '[',
+            '{': '}',
+            '}': '{',
+            '<': '>',
+            '>': '<',
+            '<': '>',
+            '>': '<'
+        };
+        return str.split('').map(char => mirroredChars[char] ? mirroredChars[char] : char).reverse().join('');
+    }
 }
 
 // Exporting the class
